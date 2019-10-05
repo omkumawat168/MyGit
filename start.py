dbd533a5805b7bd1a956fb4d4aaf8dcd79b95867
Om <omkumawat168@gmail.com>
2019-10-05 19:43:16 +0530

    start.py

---
+++ start.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import os
+import sys
+'''
+Usage: start.py <options>
+'''
+def main():
+    com = sys.argv[1]
+    print('start...',com)
+
+if __name__=='__main__':
+    main()

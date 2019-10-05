6b8f2848ec289d24fdf368ce684d6384bcecffec
omkumawat168 <omkumawat168@gmail.com>
2019-10-05 21:26:37 +0530

    sef

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 import os
 
-<<<<<<< Updated upstream
-print("hello, python",os.getenv('M2_HOME'))
-=======
+
 #this is a comment
 print("hello, python",os.getenv('PYTHON'))
->>>>>>> Stashed changes
+
